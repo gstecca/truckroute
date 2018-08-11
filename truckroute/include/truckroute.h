e1ab7f9aadca406ef5b9096d7daa81ce47b00fff
Giuseppe Stecca <giuseppe.stecca@gmail.com>
2018-08-11 21:20:24 +0200

    import data implemented and tested

--- truckroute/include/truckroute.h
+++ truckroute/include/truckroute.h
@@ -16,6 +16,7 @@
 
 
 typedef std::tuple<int, int> t_arc;
+typedef std::tuple<int, int> t_odserv; // origin - destination service
 
 struct s_cost {
 	int c; //cost
@@ -24,22 +25,39 @@ struct s_cost {
 
 
 struct trdata {
-	std::vector<int> o; // order
-	std::vector<int> o_s; // order source
-	std::vector<int> o_t; // order target
-	std::map<int,int> order_qty;
+	std::map<t_odserv, int> orders; // order
 	std::map<t_arc,s_cost> arcs; // arc cost
-	std::map<int,int> t; //arc traveling time
 	std::set<int> n; //nodes
 	int C; //max vehicle capacity
 	int k; //number of vehicles
 	int F; // fixed cost for vehicle stop
 	int source;
 	int target;
+	std::string to_string(){
+		std::string ss = "";
+		ss += "C:" + std::to_string(C)  + "\t k:" + std::to_string(k) + "\t F:" + std::to_string(F) + "\t source: " + std::to_string(source);
+		ss += "\n nodes: " + std::to_string(n.size());
+		ss += "\n arcs: " + std::to_string(arcs.size());
+		ss += "\n orders: " + std::to_string(orders.size()) + "\n";
+		ss += "arcs:\n";
+		ss += "from\tto\tcost\ttime\n";
+		for (auto a : arcs){
+			ss += std::to_string(std::get<0>(a.first)) + "\t" + std::to_string(std::get<1>(a.first))
+					+ "\t" + std::to_string(a.second.c) + "\t" + std::to_string( a.second.t) + "\n" ;
+		}
+		ss += "orders:\n";
+		ss += "from\tto\tcost\n";
+		for (auto o : orders){
+			ss += std::to_string(std::get<0>(o.first)) + "\t" + std::to_string(std::get<1>(o.first))
+					+ "\t" + std::to_string(o.second) + "\n" ;
+		}
+		return ss;
+	}
 
 };
 
 int load_csv(trdata * dat, std::string filenamebase);
+int buildmodel(IloModel* model, trdata* dat);
 
 
 
--- truckroute/src/truckroute.cpp
+++ truckroute/src/truckroute.cpp
@@ -10,28 +10,33 @@
 #include <ilcplex/ilocplex.h>
 #include <truckroute.h>
 #include <string>
-using namespace std;
+using std::cout;
+using std::cin;
+
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
 	IloEnv env;
 	IloModel model(env);
 	trdata dat;
-	string filenamebase = "input/instanzaNord1";
+	std::string filenamebase = "input/instanzaNord1";
 	load_csv(&dat, filenamebase);
+	buildmodel(&model, &dat);
+	cout << dat.to_string();
 	return 0;
 }
 
-int load_csv(trdata * dat, string filenamebase)
+int load_csv(trdata * dat, std::string filenamebase)
 //int load_data_tw(vector<int>* profits, vector<coord>* coords, int* n, int* m, float* tau, string filename)
 {
+	using std::string;
     /*
      * READING GENERAL
      */
 	string fsheet = "general";
     string filename = filenamebase + "_" + fsheet + ".csv";
 	cout << "reading " << filename << std::endl;
-	ifstream myfile(filename);
+	std::ifstream myfile(filename);
     string line;
 	string delimiter = ",";
 	string token1;
@@ -44,7 +49,7 @@ int load_csv(trdata * dat, string filenamebase)
 
         for (int i = 0; i < 5; i++){
 			getline(myfile, line);
-			cout << line << "\n";
+			//cout << line << "\n";
 			startsub = 0;
 			pos = line.find(delimiter);
 			token1 = line.substr(startsub, pos);
@@ -62,7 +67,7 @@ int load_csv(trdata * dat, string filenamebase)
 			else if (token1 == "target")
 				dat->target = stoi(token2);
 
-			cout << "READ " <<  token1 << "\n";
+			//cout << "READ " <<  token1 << "\n";
         }
         myfile.close();
     } else
@@ -75,8 +80,8 @@ int load_csv(trdata * dat, string filenamebase)
         fsheet = "network";
         filename = filenamebase + "_" + fsheet + ".csv";
         cout << "reading " << filename << std::endl;
-        ifstream myfile2(filename);
-        if(myfile){
+        std::ifstream myfile2(filename);
+        if(myfile2){
 			getline(myfile2, line); // skip first line
 			while(getline(myfile2, line)) {
 				startsub = 0;
@@ -93,120 +98,61 @@ int load_csv(trdata * dat, string filenamebase)
 				startsub = pos +1;
 				pos = line.find(delimiter, startsub);
 				float t = stof(line.substr(startsub, pos - startsub));
-				cout << from << "\t" << to << "\t"<< c << "\t" << t << "\n";
+				//cout << from << "\t" << to << "\t"<< c << "\t" << t << "\n";
 				t_arc arc = std::make_tuple(from, to);
 				s_cost arc_cost = {c, t};
 				dat->arcs[arc] = arc_cost;
-
-
 			}
+			myfile2.close();
         } else
             cout << "error opening file\n";
-
         /*
          * READING ORDERS
          */
-
-        /*
-
-
-        //
-
-         //
-
-        //cout << dat->m << "\n";
-
-        startsub = pos + 1; //
-        pos = line.find(delimiter, startsub);
-        token = line.substr(startsub, pos - startsub);
-            // coords[i][1] = stof(token);
-        dat->n  = stoi(token);
-
-        getline(myfile, line); //skip a line
-
-       // c->y = stof(token);
-            // cout << "y coord: " << token << endl;
-            // profit
-
-        int i = 0;
-
-
-
-        while(getline(myfile, line)) {
-            */
-           /*
-            * 	i x y d S f a list O C
-            *
-            *   Where
-            *   i = vertex number
-            *   x = x coordinate
-            *   y = y coordinate
-            *   d = service duration or visiting time
-            *   S = profit of the location
-            *   f = not relevant
-            *   a = not relevant
-            *   list = not relevant (length of the list depends on a)
-            *   O = opening of time window (earliest time for start of service)
-            *   C = closing of time window (latest time for start of service)
-            *
-            *    * REMARKS *
-            *    - The first point (index 0) is the starting AND ending point.
-            *    - The number of paths (P) is not included in the data file. This
-            *
-            *
-            *
-            */
-            //cout << "line: " << line << "\n";
-        	/*
-        	cout << line << "\n";
-
-
-
-            string buf; // Have a buffer string
-            stringstream ss(line); // Insert the string into a stream
-
-            vector<string> tokens; // Create vector to hold our words
-
-
-            while (ss >> buf)
-                tokens.push_back(buf);
-
-
-            */
-
-            /*cout << "LINE TOKENIZED  \n";
-            for (string aaa : tokens)
-                cout << aaa << "*";
-            cout << "\n";
-            */
-
-        	/*
-        	 *
-        	coord* c = new coord();
-            tw * ttw = new tw();
-
-            dat->id.push_back(stoi(tokens[0]));
-            float x = stof(tokens[1]);
-            float y = stof(tokens[2]);
-            c->x = x;
-            c->y = y;
-            dat->coords.push_back(*c);
-            //visit time
-            dat->s.push_back(stoi(tokens[3]));
-            // profit
-            dat->p.push_back(stoi(tokens[4]));
-            // time windows
-            ttw->l = stoi(tokens[tokens.size()-2]);
-            ttw->r = stoi(tokens[tokens.size()-1]);
-            dat->atw.push_back(*ttw);
-            if(i == 0){
-                dat->tau = ttw->r;
-                i = 1;
-            }
-
-
+        fsheet = "orders";
+        filename = filenamebase + "_" + fsheet + ".csv";
+        cout << "reading " << filename << std::endl;
+        std::ifstream myfile3(filename);
+        if (myfile3){
+        	getline(myfile3, line); // skipping first line
+        	while(getline(myfile3, line)){
+        		startsub = 0;
+        		pos = line.find(delimiter);
+        		int from = stoi(line.substr(startsub, pos));
+        		startsub = pos + 1;
+        		pos = line.find(delimiter, startsub);
+        		int to = stoi(line.substr(startsub, pos - startsub));
+        		startsub = pos + 1;
+        		pos = line.find(delimiter, startsub);
+        		int d = stoi(line.substr(startsub, pos - startsub));
+        		t_odserv od = std::make_tuple(from, to);
+        		if (dat->orders[od])
+        			dat->orders[od] = dat->orders[od] + d;
+        		else
+        			dat->orders[od] = d;
+        		//cout << from << "\t" << to << "\t"<< d << "\n";
+        	}
         }
-        */
+
 
     return 0;
+}
+
+int buildmodel(IloModel* model, trdata* dat) {
+	using std::string;
+	int n = dat->n.size();
+	int m = dat->k;
+	IloEnv env = model->getEnv();
+    IloBoolVar x[n+2][n+2][m];
+    for(int r = 0; r < n+2; r++) {
+        for(int c = 0; c < n+2; c++) {
+            for(int k=0; k<m; k++) {
+                string varName = "x#" + std::to_string(r) + "#" + std::to_string(c) + "#" + std::to_string(k);
+                x[r][c][k] = IloBoolVar(env, 0, 1, varName.c_str());
+                //vars[varName] = x[r][c][k];
+            }
+
+        }
+    }
+	return 1;
 }
